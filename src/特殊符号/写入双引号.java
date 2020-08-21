c41ae225663cd27a9ba70100292f2348012fd4c7
xiaoyangpeng <a1171238267@gmail.com>
2020-08-21 19:04:14 +0200

    特殊符号

---
+++ src/特殊符号/写入双引号.java
@@ -0,0 +1,11 @@
+package ÌØÊâ·ûºÅ;
+
+public class Ğ´ÈëË«ÒıºÅ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\"o\"");// "o"
+		System.out.println("\"\"");  // ""
+	}
+
+}
---
+++ src/特殊符号/换行.java
@@ -0,0 +1,13 @@
+package 特殊符号;
+
+public class 换行 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("dfasdfadfad");
+System.out.print("\r\n");//这里是print下一行应该写后面的但是现在换行了
+System.out.println("sdfvsadfasdfasdga");
+	}
+
+}
